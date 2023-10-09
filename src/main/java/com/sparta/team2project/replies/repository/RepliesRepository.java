6966728985f3b744d12fc0928ec25d8bf3279183
kimyounggeul <129300980+kimyounggeul@users.noreply.github.com>
2023-10-09 11:42:48 +0900

    feat : comments 및 replies 수정 중
    중간커밋
    정환님 Post 최신꺼 병합본

--- src/main/java/com/sparta/team2project/replies/repository/RepliesRepository.java
+++ src/main/java/com/sparta/team2project/replies/repository/RepliesRepository.java
@@ -1,5 +1,6 @@
 package com.sparta.team2project.replies.repository;
 
+import com.sparta.team2project.comments.entity.Comments;
 import com.sparta.team2project.replies.entity.Replies;
 import org.springframework.data.jpa.repository.JpaRepository;
 
@@ -11,6 +12,4 @@ public interface RepliesRepository extends JpaRepository<Replies, Long> {
     List<Replies> findByIdComments_IdOrderByCreatedAtDesc(Long commentId);
 
     List<Replies> findAllByCommentsOrderByCreatedAtDesc(Comments comments);
-
-    List<Replies> findByCommentIdAndPostIdOrderByCreatedAtDesc(Long commentId, Long postId);
 }
