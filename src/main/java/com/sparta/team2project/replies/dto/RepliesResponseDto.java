58d001ebf2eb38c542724b8a6e389e55cabfbb70
kimyounggeul <129300980+kimyounggeul@users.noreply.github.com>
2023-10-24 20:45:13 +0900

    refactor : Comments & Replies 조회시 글쓴이 확인되게 변경
    수정 완료

--- src/main/java/com/sparta/team2project/replies/dto/RepliesResponseDto.java
+++ src/main/java/com/sparta/team2project/replies/dto/RepliesResponseDto.java
@@ -1,15 +1,18 @@
 package com.sparta.team2project.replies.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.sparta.team2project.replies.entity.Replies;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RepliesResponseDto {
     private Long repliesId;
     private String contents;
     private String email;
+    private String checkUser;
     private LocalDateTime createAt;
     private LocalDateTime modifiedAt;
 
@@ -19,6 +22,15 @@ public class RepliesResponseDto {
         this.createAt = replies.getCreatedAt();
         this.modifiedAt = replies.getModifiedAt();
         this.email = users;
+    }
+
+    public RepliesResponseDto(Replies replies, String users, String checkUser) {
+        this.repliesId = replies.getId();
+        this.contents = replies.getContents();
+        this.createAt = replies.getCreatedAt();
+        this.modifiedAt = replies.getModifiedAt();
+        this.email = users;
+        this.checkUser = checkUser;
     }
 }
 
