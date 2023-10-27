13a2cb95b8cb8830d55b51fbf8dcb2187c354c95
JeongHwan Kim <rocky990903@naver.com>
2023-10-27 18:56:58 +0900

    Upadte.전체 게시글 및 상세 게시글 조회시 프로필사진 추가
    Upadte.전체 게시글 및 상세 게시글 조회시 프로필사진 추가

--- src/main/java/com/sparta/team2project/posts/dto/TotalRequestDto.java
+++ src/main/java/com/sparta/team2project/posts/dto/TotalRequestDto.java
@@ -1,11 +1,14 @@
 package com.sparta.team2project.posts.dto;
 
 import com.sparta.team2project.posts.entity.PostCategory;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
 @Getter
+@AllArgsConstructor
 public class TotalRequestDto {
     private String title;
     private String contents;
