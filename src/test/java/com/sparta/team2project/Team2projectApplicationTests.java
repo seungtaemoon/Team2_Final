a8c7378071e9ff4286db057259243c0eaf0f79b5
문승태 <conanmoon@gmail.com>
2023-11-01 01:57:57 +0900

    Update Team2projectApplicationTests.java

--- src/test/java/com/sparta/team2project/Team2projectApplicationTests.java
+++ src/test/java/com/sparta/team2project/Team2projectApplicationTests.java
@@ -3,7 +3,7 @@ package com.sparta.team2project;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Team2projectApplicationTests.class)
 class Team2projectApplicationTests {
 
 	@Test
